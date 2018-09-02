41fef0d6b70c1012f0cd0c28c44c92c991c06bed
Yahia Bakour <yehia234@bu.edu>
2018-09-02 08:19:36 -0400

    BFS and DFS in python with graph edge list

---
+++ Breadth_First_Search.py
@@ -0,0 +1,84 @@
+graph = {'1': set(['2', '3']),
+         '2': set(['1', '4', '5']),
+         '3': set(['1', '6']),
+         '4': set(['2']),
+         '5': set(['2', '6']),
+         '6': set(['3', '5'])}
+
+print(graph)
+
+print(graph.keys())
+
+print(graph.values())
+
+from collections import deque
+def bfs(graph,start): #Normal BFS for components
+    visited, queue = set(), deque([start]) #Use Deque for popleft
+    while queue:
+        vertex = queue.popleft() #for O(1) pop
+        
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+            if(len(queue) == 1):
+                print(vertex)
+            else:
+                print(vertex,end=" --> ")
+    return visited
+
+
+from collections import deque
+def bfs_paths(graph,start,goal):
+    queue = deque([(start,[start])]) # Queue = [(start vertex,[path to goal from start vertex])]
+    paths = [] #Contains all paths from start vertex to end vertex
+    while(queue):
+        (vertex,path) = queue.popleft()
+        for nxt in graph[vertex] - set(path):
+            if nxt == goal:
+                paths.append(path+[nxt])
+            else:
+                queue.append((nxt,path+[nxt]))
+    return paths
+
+
+"""
+Graph implemented with lists instead of sets
+"""
+graph = {'1': ['2', '3'],
+         '2': ['1', '4', '5'],
+         '3': ['1', '6'],
+         '4': ['2'],
+         '5': ['2', '6'],
+         '6': ['3', '5']}
+
+
+from collections import deque
+def bfs(graph,start): #Normal Bfs for components
+    visited, queue = [], deque([start])
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            visited.append(vertex)
+            queue.extend([i for i in graph[vertex] if i not in visited])
+    return visited
+
+
+
+dfs(graph,'1')
+
+from collections import deque
+def bfs_paths(graph,start,goal):
+    queue = deque([(start,[start])]) # Stack contains a set (current vertex (initially start), [path to end vertex from start vertex])
+    paths = []
+    while(queue):
+        (vertex,path) = queue.popleft()
+        for nextvertex in graph[vertex]:
+            if(nextvertex not in path):
+                if nextvertex == goal:
+                    paths.append(path + [nextvertex])
+                else:
+                    queue.append([nextvertex, path + [nextvertex]])
+    return paths
+
+dfs_paths(graph,'1','6')
+
---
+++ Depth_First_Search.py
@@ -0,0 +1,84 @@
+graph = {'1': set(['2', '3']),
+         '2': set(['1', '4', '5']),
+         '3': set(['1', '6']),
+         '4': set(['2']),
+         '5': set(['2', '6']),
+         '6': set(['3', '5'])}
+
+print(graph)
+
+print(graph.keys())
+
+print(graph.values())
+
+def dfs(graph, start): #Normal DFS for components
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+            if(len(stack) == 1):
+                print(vertex)
+            else:
+                print(vertex,end=" --> ")
+    return visited
+
+dfs(graph,'1')
+
+
+def dfs_paths(graph, start, end): #To find and return all paths from start vertex to end vertex
+    stack = [(start, [start])]
+    paths = []
+    while stack:
+        (vertex, path) = stack.pop()
+        for nxt in graph[vertex] - set(path):
+            if nxt == end:
+                paths.append(path + [nxt])
+            else:
+                stack.append((nxt, path + [nxt]))
+    return paths
+
+dfs_paths(graph,'1','6')
+
+
+"""
+Graph implemented with lists instead of sets
+"""
+graph = {'1': ['2', '3'],
+         '2': ['1', '4', '5'],
+         '3': ['1', '6'],
+         '4': ['2'],
+         '5': ['2', '6'],
+         '6': ['3', '5']}
+
+
+def dfs(graph, start): #Normal DFS for components
+    visited, stack = [], [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.append(vertex)
+            stack.extend([i for i in graph[vertex] if i not in visited])
+    return visited
+
+
+
+dfs(graph,'1')
+
+
+def dfs_paths(graph, start, goal): #DFS TO FIND ALL PATHS TO ENDGOAl
+    stack = [(start,[start])] # Stack contains a set (current vertex (initially start), [path to end vertex from start vertex])
+    paths = []
+    while(stack):
+        (vertex,path) = stack.pop()
+        for nextvertex in graph[vertex]:
+            if(nextvertex not in path):
+                if nextvertex == goal:
+                    paths.append(path + [nextvertex])
+                else:
+                    stack.append([nextvertex, path + [nextvertex]])
+    return paths
+
+dfs_paths(graph,'1','6')
+
